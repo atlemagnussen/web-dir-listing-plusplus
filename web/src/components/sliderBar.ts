7301c7feb6f0e6139d843c242c8d26395046db74
Atle Magnussen <atlemagnussen@gmail.com>
2023-03-17 11:22:53 +0100

    now

--- web/src/components/sliderBar.ts
+++ web/src/components/sliderBar.ts
@@ -1,5 +1,5 @@
 import { LitElement, css, html } from "lit"
-import { customElement, property, state } from "lit/decorators.js"
+import { customElement, property } from "lit/decorators.js"
 
 @customElement('slider-bar')
 export class SliderBar extends LitElement {

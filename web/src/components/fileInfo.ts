66b78d3afc36b69a992145d865697cb3c78e0319
Atle Magnussen <atlemagnussen@gmail.com>
2023-05-03 21:40:06 +0200

    add dl

--- web/src/components/fileInfo.ts
+++ web/src/components/fileInfo.ts
@@ -7,7 +7,28 @@ import { customElement, property } from "lit/decorators.js"
 export class FileInfo extends LitElement {
     static styles = css`
         :host {
-            
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            max-width: 100%;
+            overflow: none;
+            --button-height: 3rem;
+            --button-width: 3rem;
+        }
+        * {
+            box-sizing: border-box;
+        }
+        .wrapper {
+            padding: 0.5rem;
+            gap: 0.5rem;
+            display: flex;
+            flex-direction: column;
+            width: var(--default-width);
+            max-width: var(--default-width);
+            border: 1px solid var(--link-color);
+        }
+        file-ext-label {
+            font-size: 1.4rem;
         }
     `
 
@@ -16,7 +37,24 @@ export class FileInfo extends LitElement {
 
     render() {
         return html`
-            <span title="${this.file.name}">${this.file.name}</span>
+            <div class="wrapper"> 
+                <div class="section">
+                    <label title="${this.file.name}">${this.file.name}</label>
+                </div>
+                <div class="section">
+                    <div class="information">
+                        
+                        <div class="fileinfo">
+                            ${this.file.ext ? html`
+                                <file-ext-label ext="${this.file.ext}"></file-ext-label>
+                                <a href="${this.file.path}" download filename="${this.file.name}">
+                                    <download-button></download-button>
+                                </a>
+                            ` : ""}
+                        </div>
+                    </div>
+                </div>
+            </div>
         `
     }
 }

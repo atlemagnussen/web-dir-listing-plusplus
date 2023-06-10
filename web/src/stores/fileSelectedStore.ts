2bbb95a741c68dbd90798a8dfb0433838ea12ab9
Atle Magnussen <atlemagnussen@gmail.com>
2023-06-10 12:23:30 +0200

    rem debu

--- web/src/stores/fileSelectedStore.ts
+++ web/src/stores/fileSelectedStore.ts
@@ -1,7 +1,7 @@
 import { ConfigFolder, FileOrDir, PlayingState } from "@common/types"
 import { BehaviorSubject } from "rxjs"
 
-const PlayingSubject = new BehaviorSubject<FileOrDir>({name: "1", ext: "mp3", size: 0, path: "1", type: "file"})
+const PlayingSubject = new BehaviorSubject<FileOrDir>({name: "", ext: "", size: 0, path: "", type: "file"})
 export const playingFile = PlayingSubject.asObservable()
 export const setPlayingFile = (file: FileOrDir) => PlayingSubject.next(file)
 

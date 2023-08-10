6c141684a76541bf5e3ee6fda8291792a190fcde
Atle Magnussen <atlemagnussen@gmail.com>
2023-08-10 08:24:25 +0200

    sd

--- server/config.ts
+++ server/config.ts
@@ -12,7 +12,7 @@ const config = dotenv.config({ path: envFilePath})
 console.log(config)
 
 const portStr = process.env.PORT as string
-const port = portStr ? 8000 : parseInt(portStr)
+const port = portStr ? parseInt(portStr) : 8000
 
 const title = process.env.TITLE ? process.env.TITLE : "Dir list++"
 

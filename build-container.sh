c84bfb85aa8031233c95735faab2729193576f86
Atle Magnussen <atlemagnussen@gmail.com>
2025-07-12 17:33:31 +0200

    b

--- build-container.sh
+++ build-container.sh
@@ -1,1 +1,1 @@
-podman build -f Containerfile --tag atlmag/web-dir-listing:2.8 --tag atlmag/web-dir-listing:latest
+podman build -f Containerfile --tag atlmag/web-dir-listing:2.10 --tag atlmag/web-dir-listing:latest

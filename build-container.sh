2203006ae6c57a3e53c96b06dcb7fd46188d837a
Atle Magnussen <atlemagnussen@gmail.com>
2026-02-15 15:39:03 +0100

    build

--- build-container.sh
+++ build-container.sh
@@ -3,7 +3,6 @@
 IMAGE_NAME="atlmag/web-dir-listing"
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-
 echo "Podman Build $IMAGE_NAME:$COMMIT_HASH"
 
 podman build -f Containerfile --tag "$IMAGE_NAME:$COMMIT_HASH" --tag "$IMAGE_NAME:latest"
--- build-fileserver.sh
+++ build-fileserver.sh
@@ -1,1 +1,7 @@
-podman build -f Containerfile-server --tag atlmag/web-dir-listing.net:latest
+#!/usr/bin/env bash
+
+IMAGE_NAME="atlmag/web-dir-listing.net"
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
+
+podman build -f Containerfile-server --tag "$IMAGE_NAME:$COMMIT_HASH" --tag "$IMAGE_NAME:latest"
